4a2e6dd7e934ed61605b300ad637134168ca146a
Ivan Pratasau <ivanprotasov@narod.ru>
2021-05-04 22:27:31 +0300

    Add solution for 5-module 2-task (and some fixes for 5-module 1-task)

--- 05-http-server-streams/02-file-server-post/server.js
+++ 05-http-server-streams/02-file-server-post/server.js
@@ -1,22 +1,75 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
+const maxSize = 1048576;
 
-server.on('request', (req, res) => {
-  const pathname = url.parse(req.url).pathname.slice(1);
+server.on('request', async (req, res) => {
+  try {
+    const pathname = url.parse(req.url).pathname.slice(1);
+    const filepath = path.join(__dirname, 'files', pathname);
+    if (!fs.existsSync('files')) {
+      fs.mkdirSync('files');
+    }
+    switch (req.method) {
+      case 'POST':
+        if (pathname.indexOf('/') >= 0) {
+          res.statusCode = 400;
+          return res.end();
+        }
+        fs.access(filepath, (err) => {
+          if (!err) {
+            res.statusCode = 409;
+            return res.end();
+          }
+          let size = 0;
+          const body = [];
+          let isLimit;
 
-  const filepath = path.join(__dirname, 'files', pathname);
+          req.on('data', function(data) {
+            size += data.length;
+            body.push(data);
 
-  switch (req.method) {
-    case 'POST':
+            if (size > maxSize) {
+              isLimit = true;
+              res.statusCode = 413;
+              return res.end('File is too big!');
+            }
+          });
 
-      break;
+          req.on('error', (e) => {
+            console.error(e.stack);
+            res.statusCode = 500;
+            res.end();
+          });
 
-    default:
-      res.statusCode = 501;
-      res.end('Not implemented');
+          req.on('end', ()=>{
+            if (!isLimit) {
+              const file = fs.createWriteStream(filepath, {autoDestroy: true});
+              file.on('error', function(e) {
+                console.error(e.stack);
+                res.statusCode = 500;
+                res.end();
+              }).write(Buffer.concat(body));
+              file.on('finish', ()=>{
+                res.statusCode = 201;
+                return res.end();
+              });
+              file.end();
+            }
+          });
+        });
+        break;
+      default:
+        res.statusCode = 501;
+        res.end('Not implemented');
+    }
+  } catch (e) {
+    console.error(e.stack);
+    res.statusCode = 500;
+    res.end();
   }
 });
 
